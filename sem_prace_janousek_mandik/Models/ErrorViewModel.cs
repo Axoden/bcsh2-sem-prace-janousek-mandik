1e66253dd5f7aecea80d54826f95cdce45a28b23
Axoden <janousek523@gmail.com>
2023-10-02 14:58:55 +0200

    Add project files.

---
+++ sem_prace_janousek_mandik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sem_prace_janousek_mandik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
